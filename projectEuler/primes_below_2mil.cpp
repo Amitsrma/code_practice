96776bf2c84316f9c74caa8b9772a4641804a2d8
Amit Sharma <45700624+Amitsrma@users.noreply.github.com>
2020-10-23 19:20:10 -0400

    Add files via upload

---
+++ projectEuler/primes_below_2mil.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+long long int primes(){
+	long int toCheck = 2;
+	long int Divisors[500000];
+	Divisors[0] = 2;
+	Divisors[1] = 3;
+	int length_divisors = 2;
+	long long int out = 2+3;
+	while (toCheck < 2000000) {
+		long int undivided = 0;
+		for (int indx = 0; indx < length_divisors; indx++){
+			if (toCheck%Divisors[indx] != 0){
+				undivided++;
+			}
+			else
+				break;
+		}
+
+		if (undivided == length_divisors){
+			Divisors[length_divisors] = toCheck;
+			length_divisors++;
+			out += toCheck;
+			//if (toCheck > 900000)
+			//	cout << toCheck << " ";
+		}
+		toCheck++;
+	}
+
+	cout <<"\n\n" << Divisors[length_divisors-3] << " " << Divisors[length_divisors-2] << " " << Divisors[length_divisors-1];
+
+	return out;
+}
+
+int main(){
+	cout << "Process Initiated! \n";
+    clock_t tStart = clock();
+	cout << "\n\nThe sum of primes is: " << primes();
+    printf("\n\nTime taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+	return 0;
+}
