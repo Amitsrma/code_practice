c0ecd1bea3dafe3708607871b7b39dfb8aa2564b
Amit Sharma <45700624+Amitsrma@users.noreply.github.com>
2020-11-02 23:04:56 -0500

    Update Fraudulent Activity Notifications.py

--- HR/Fraudulent Activity Notifications.py
+++ HR/Fraudulent Activity Notifications.py
@@ -4,59 +4,30 @@ import bisect
 def median(arr, n):
     return (arr[n//2]+arr[n//2-1])/2 if n%2==0 else arr[(n+1)//2-1]
 
-
-def check_process_flow1(expenditure, d):
-    begin = expenditure[:d]
-    begin.sort()
-    to_check = expenditure[d]
-    mid = median(begin, d)
-    num_notification = 0
-    if to_check >= 2*mid:
-        print("notified!")
-        num_notification += 1
-    n = len(expenditure)
-    hashes_s = {}
-    for index, i in enumerate(begin):
-        try:
-            hashes_s[i].append(index)
-        except:
-            hashes_s[i] = [index,]
-    start = 0
-    for i in range(d+1, n):
-        to_check = expenditure[i]
-        to_add = expenditure[i-1]
-        to_remove = expenditure[start]
-        start += 1
-        begin.pop(bisect.bisect_left(begin, to_remove))
-        bisect.insort_left(begin, to_add)
-
-        if median(begin, d)*2 <= to_check:
-            num_notification += 1
-    print(num_notification)
-
-
 def activityNotifications(expenditure, d):
+    hashes_s = {}
+    to_check = expenditure[d]
+    n = len(expenditure)
+    num_notification = 0
+    
     begin = expenditure[:d]
     begin.sort()
-    to_check = expenditure[d]
-    mid = median(begin, d)
-    num_notification = 0
-    if to_check >= 2*mid:
-        print("notified!")
+
+    if to_check >= 2*median(begin, d):
         num_notification += 1
-    n = len(expenditure)
-    hashes_s = {}
+    
     for index, i in enumerate(begin):
         try:
             hashes_s[i].append(index)
         except:
             hashes_s[i] = [index,]
-    start = 0
+    
     for i in range(d+1, n):
         to_check = expenditure[i]
         to_add = expenditure[i-1]
         to_remove = expenditure[start]
-        start += 1
+        #using bisect to add element to a previously sorted array
+        #this is faster
         begin.pop(bisect.bisect_left(begin, to_remove))
         bisect.insort_left(begin, to_add)
 
